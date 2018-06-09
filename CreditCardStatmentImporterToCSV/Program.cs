8b9908e2a88b6c3f2c5281ad03ad7e66f5908fed
Chito Leung <ctleung@yahoo.com>
2018-06-09 07:23:14 -0700

    Test My First Commit

--- CreditCardStatmentImporterToCSV/Program.cs
+++ CreditCardStatmentImporterToCSV/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace CreditCardStatmentImporterToCSV
 {
@@ -10,6 +12,34 @@ namespace CreditCardStatmentImporterToCSV
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Usage: Use filename as argument");
+
+                string[] lines = File.ReadAllLines(args[0]);
+                string[] newLines = new string[lines.Length];
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    char[] sep = { ' ' };
+                    string[] token = lines[i].Split(sep, StringSplitOptions.RemoveEmptyEntries);
+                    newLines[i] = token[0] + ",";
+                    for (int j = 1; j < token.Length - 1; j++)
+                    {
+                        newLines[i] += token[j];
+                        newLines[i] += " ";
+                    }
+                    newLines[i] += ",-";
+                    newLines[i] += token[token.Length - 1];
+                }
+
+                string fileNameNoExt = System.IO.Path.GetFileNameWithoutExtension(args[0]);
+                string csvPath = Path.Combine(System.IO.Path.GetDirectoryName(args[0]), fileNameNoExt + ".csv");
+                File.WriteAllLines(csvPath, newLines);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
